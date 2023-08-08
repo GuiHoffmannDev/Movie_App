c38a53aa1c6d6e55bad5ef695ee2d32e844dc4f0
GuiHoffmannDev <guihoffmann.dev@outlook.com>
2023-08-08 20:08:44 -0300

    Initial Commit

---
+++ MovieBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  MovieBooking
+//
+//  Created by Guilherme on 08/08/23.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
